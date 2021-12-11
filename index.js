928296eb4b3cac98909cebd30843d12c188204fc
ivoraphael <raphaelivo.net@gmail.com>
2021-12-11 15:42:49 -0300

    ...

--- index.js
+++ index.js
@@ -3,6 +3,32 @@
 ////    return true;
 ////}
 
+// Code to handle install prompt on desktop
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    addBtn.addEventListener('click', () => {
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
+
 const swalWithBootstrapButtons = Swal.mixin({
     customClass: {
         confirmButton: 'btn btn-success',
@@ -23,9 +49,7 @@ $(document).ready(function () {
     }).then((result) => {
         if (result.isConfirmed) {
 
-            navigator.serviceWorker
-                .register('sw.js')
-                .then(() => { console.log('Service Worker Registered'); });
+            $('#addButton').click();
 
         } else if (
             result.dismiss === Swal.DismissReason.cancel
