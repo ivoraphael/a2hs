08dee53d8809056cb483ce0522845358960fca57
ivoraphael <raphaelivo.net@gmail.com>
2021-12-11 15:05:59 -0300

    Adicionado alerta para permissão de atalho

--- index.js
+++ index.js
@@ -10,25 +10,54 @@ let deferredPrompt;
 
 })();
 
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+})
+
 function teste() {
 
-    Swal.fire('Service Worker Registered');
 
-    //// Prevent Chrome 67 and earlier from automatically showing the prompt
-    //e.preventDefault();
-    //// Stash the event so it can be triggered later.
-    //deferredPrompt = e;
-    //// Update UI to notify the user they can add to home screen
-    //// Show the prompt
-    //deferredPrompt.prompt();
-    //// Wait for the user to respond to the prompt
-    //deferredPrompt.userChoice.then((choiceResult) => {
-    //    if (choiceResult.outcome === 'accepted') {
-    //        console.log('User accepted the A2HS prompt');
-    //    } else {
-    //        console.log('User dismissed the A2HS prompt');
-    //    }
-    //    deferredPrompt = null;
-    //});
+    swalWithBootstrapButtons.fire({
+        title: 'Adicionar à tela inicial?',
+        text: "Sua escolha ficará salva!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Adicionar atalho!',
+        cancelButtonText: 'Não adicionar atalho.',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            a2hs();
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            Swal.fire('Atalho não adicionado!');
+        }
+    })
+}
+
+function a2hs() {
+
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        } else {
+            console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+    });
 
 }
