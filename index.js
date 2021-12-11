47d1c4e3f85a2e8f432feb2a60389530743fdb72
ivoraphael <raphaelivo.net@gmail.com>
2021-12-11 15:30:31 -0300

    changes

--- index.js
+++ index.js
@@ -31,19 +31,6 @@ const swalWithBootstrapButtons = Swal.mixin({
 
 (function () {
 
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('sw.js')
-            .then(() => { console.log('Service Worker Registered'); });
-    }
-
-    window.addEventListener('beforeinstallprompt', (e) => {
-        // Prevent Chrome 67 and earlier from automatically showing the prompt
-        e.preventDefault();
-        // Stash the event so it can be triggered later.
-        deferredPrompt = e;
-    });
-
     swalWithBootstrapButtons.fire({
         title: 'Adicionar atalho em sua tela inicial?',
         text: "Sua escolha ficara salva!",
