2407a142e3044d33cde8fc1dd2bb38fe2ef5b222
ivoraphael <raphaelivo.net@gmail.com>
2021-12-11 15:01:32 -0300

    changed registration

--- sw.js
+++ sw.js
@@ -4,11 +4,7 @@ self.addEventListener('install', (e) => {
       '',
       'index.html',
       'index.js',
-      'style.css',
-      'images/fox1.jpg',
-      'images/fox2.jpg',
-      'images/fox3.jpg',
-      'images/fox4.jpg',
+      'style.css'
     ])),
   );
 });
