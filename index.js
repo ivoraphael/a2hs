0d145d3a51d34e72ff534f8ea534fa7efaa22478
ivoraphael <raphaelivo.net@gmail.com>
2021-12-11 15:38:08 -0300

    ,,,

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const swalWithBootstrapButtons = Swal.mixin({
     buttonsStyling: false
 })
 
-(function () {
-
+$(document).ready(function () {
     swalWithBootstrapButtons.fire({
         title: 'Adicionar atalho em sua tela inicial?',
         text: "Sua escolha ficara salva!",
@@ -28,44 +27,10 @@ const swalWithBootstrapButtons = Swal.mixin({
                 .register('sw.js')
                 .then(() => { console.log('Service Worker Registered'); });
 
-            //// Register service worker to control making site work offline
-            //if ('serviceWorker' in navigator) {
-            //    navigator.serviceWorker
-            //        .register('sw.js')
-            //        .then(() => { console.log('Service Worker Registered'); });
-            //}
-
-            //// Code to handle install prompt on desktop
-            //let deferredPrompt;
-
-            //window.addEventListener('beforeinstallprompt', (e) => {
-            //    // Prevent Chrome 67 and earlier from automatically showing the prompt
-            //    e.preventDefault();
-            //    // Stash the event so it can be triggered later.
-            //    deferredPrompt = e;
-            //});
-
-            //// Show the prompt
-            //deferredPrompt.prompt();
-
-            //// Wait for the user to respond to the prompt
-            //deferredPrompt.userChoice.then((choiceResult) => {
-            //    if (choiceResult.outcome === 'accepted') {
-            //        console.log('User accepted the A2HS prompt');
-            //    } else {
-            //        console.log('User dismissed the A2HS prompt');
-            //    }
-            //    deferredPrompt = null;
-            //});
-
         } else if (
-            /* Read more about handling dismissals below */
             result.dismiss === Swal.DismissReason.cancel
         ) {
             Swal.fire('Atalho nao adicionado!');
         }
     })
-
-})();
-
-//////////////////////////////////////////////////////////////////
+});
