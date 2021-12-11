6625487ef351a3254ae43f113f23402f3842f9ff
ivoraphael <raphaelivo.net@gmail.com>
2021-12-11 15:33:17 -0300

    test

--- index.js
+++ index.js
@@ -31,28 +31,28 @@ const swalWithBootstrapButtons = Swal.mixin({
                     .then(() => { console.log('Service Worker Registered'); });
             }
 
-            // Code to handle install prompt on desktop
-            let deferredPrompt;
+            //// Code to handle install prompt on desktop
+            //let deferredPrompt;
 
-            window.addEventListener('beforeinstallprompt', (e) => {
-                // Prevent Chrome 67 and earlier from automatically showing the prompt
-                e.preventDefault();
-                // Stash the event so it can be triggered later.
-                deferredPrompt = e;
-            });
+            //window.addEventListener('beforeinstallprompt', (e) => {
+            //    // Prevent Chrome 67 and earlier from automatically showing the prompt
+            //    e.preventDefault();
+            //    // Stash the event so it can be triggered later.
+            //    deferredPrompt = e;
+            //});
 
-            // Show the prompt
-            deferredPrompt.prompt();
+            //// Show the prompt
+            //deferredPrompt.prompt();
 
-            // Wait for the user to respond to the prompt
-            deferredPrompt.userChoice.then((choiceResult) => {
-                if (choiceResult.outcome === 'accepted') {
-                    console.log('User accepted the A2HS prompt');
-                } else {
-                    console.log('User dismissed the A2HS prompt');
-                }
-                deferredPrompt = null;
-            });
+            //// Wait for the user to respond to the prompt
+            //deferredPrompt.userChoice.then((choiceResult) => {
+            //    if (choiceResult.outcome === 'accepted') {
+            //        console.log('User accepted the A2HS prompt');
+            //    } else {
+            //        console.log('User dismissed the A2HS prompt');
+            //    }
+            //    deferredPrompt = null;
+            //});
 
         } else if (
             /* Read more about handling dismissals below */
