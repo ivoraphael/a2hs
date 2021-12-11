60f863b87fbb4b7659dcea1a99c03b7ead8629d7
ivoraphael <raphaelivo.net@gmail.com>
2021-12-11 14:33:50 -0300

    changes to make button always visible

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ setInterval(() => {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/pwa-examples/a2hs/sw.js')
+    .register('sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
@@ -23,7 +23,7 @@ if ('serviceWorker' in navigator) {
 
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+addBtn.style.display = 'block';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -35,7 +35,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
   addBtn.addEventListener('click', () => {
     // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
+    addBtn.style.display = 'block';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
